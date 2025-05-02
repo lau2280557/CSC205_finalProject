f2da63e93e05c7762f209b5506a8ef3b88e7bed4
Lauren <laurenmtech@gmail.com>
2025-05-01 19:05:11 -0700

    aggregator things

--- aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
+++ aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -21,6 +21,7 @@ public class AggregatorController {
         this.aggregatorService = aggregatorService;
     }
 
+
     @GetMapping("/")
     public List<Entry> helloWorld() {
 
@@ -43,6 +44,48 @@ public class AggregatorController {
         String message = new StringBuilder()
                 .append("Retrieved entry for [")
                 .append(word)
+                .append("] in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+        logger.info(message);
+
+        return entry;
+    }
+
+    @GetMapping("getWordsThatContainSuccessiveLetterAndStartsWith/{chars}")
+    public List<Entry> getWordsThatContainSuccessiveLetterAndStartsWith(@PathVariable String chars) {
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        List<Entry> entry = aggregatorService.getWordsThatContainSuccessiveLettersAndStartsWith(chars);
+        stopWatch.stop();
+
+        long nanoSeconds = stopWatch.getLastTaskTimeNanos();
+        String message = new StringBuilder()
+                .append("Retrieved entry for words with successive letters that contain [")
+                .append(chars)
+                .append("] in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+        logger.info(message);
+
+        return entry;
+    }
+
+    @GetMapping("getWordsThatContain/{chars}")
+    public List<Entry> getWordsThatContain(@PathVariable String chars) {
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        List<Entry> entry = aggregatorService.getWordsThatContain(chars);
+        stopWatch.stop();
+
+        long nanoSeconds = stopWatch.getLastTaskTimeNanos();
+        String message = new StringBuilder()
+                .append("Retrieved entries for words that contain [")
+                .append(chars)
                 .append("] in ")
                 .append(nanoSeconds / 1000000.0)
                 .append("ms")
--- aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
+++ aggregator/src/main/java/com/example/aggregator/service/AggregatorService.java
@@ -4,6 +4,9 @@ import com.example.aggregator.client.AggregatorRestClient;
 import com.example.aggregator.model.Entry;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AggregatorService {
 
@@ -14,7 +17,22 @@ public class AggregatorService {
     }
 
     public Entry getDefinitionFor(String word) {
+
         return aggregatorRestClient.getDefinitionFor(word);
+    }
+
+    public List<Entry> getWordsThatContainSuccessiveLettersAndStartsWith(String chars) {
+        List<Entry> wordsThatStartWith = aggregatorRestClient.getWordsStartingWith(chars);
+        List<Entry> wordsThatContainConsecutiveLetters = aggregatorRestClient.getWordsThatContainConsecutiveLetters();
+
+        List<Entry> common = wordsThatStartWith.stream()
+                .filter(wordsThatContainConsecutiveLetters::contains)
+                .collect(Collectors.toList());
+        return common;
+    }
+
+    public List<Entry> getWordsThatContain(String chars) {
+        return aggregatorRestClient.getWordsThatContain(chars);
     }
 
 }
